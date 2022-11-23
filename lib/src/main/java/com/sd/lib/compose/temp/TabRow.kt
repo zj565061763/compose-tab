f272c443cd6f2c8f8b5cc6afe74d293d5aa33c7c
565061763 <565061763@qq.com>
2022-11-24 02:20:09 +0800

    FScrollableTabRow

--- lib/src/main/java/com/sd/lib/compose/temp/TabRow.kt
+++ lib/src/main/java/com/sd/lib/compose/temp/TabRow.kt
@@ -1,9 +1,6 @@
 package com.sd.lib.compose.temp
 
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TabPosition
-import androidx.compose.material3.TabRow
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.UiComposable
@@ -35,6 +32,38 @@ fun FTabRow(
         modifier = modifier,
         containerColor = containerColor,
         contentColor = contentColor,
+        indicator = indicator,
+        divider = divider,
+        tabs = tabs,
+    )
+}
+
+@Composable
+fun FScrollableTabRow(
+    selectedTabIndex: Int,
+    modifier: Modifier = Modifier,
+    containerColor: Color = Color.Transparent,
+    contentColor: Color = MaterialTheme.colorScheme.onSurface,
+    edgePadding: Dp = 0.dp,
+    indicator: @Composable (tabPositions: List<TabPosition>) -> Unit = @Composable { tabPositions ->
+        FTabIndicator(
+            tabPositions = tabPositions,
+            selectedTabIndex = selectedTabIndex,
+            width = 20.dp,
+        )
+    },
+    divider: @Composable @UiComposable () -> Unit =
+        @Composable {
+            Divider(thickness = Dp.Hairline)
+        },
+    tabs: @Composable @UiComposable () -> Unit,
+) {
+    ScrollableTabRow(
+        selectedTabIndex = selectedTabIndex,
+        modifier = modifier,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        edgePadding = edgePadding,
         indicator = indicator,
         divider = divider,
         tabs = tabs,
